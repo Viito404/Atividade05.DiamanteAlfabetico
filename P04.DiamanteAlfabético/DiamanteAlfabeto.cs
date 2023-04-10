27c67a62020a9405bed9220bfcefae6d5dbaff5e
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-04-10 15:16:37 -0300

    Refatoração utilizando Orientação a Objetos.

---
+++ P04.DiamanteAlfabético/DiamanteAlfabeto.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04.DiamanteAlfabético
+{
+     internal class DiamanteAlfabeto
+     {
+          public string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+          public char[] letraAlfabeto;
+          public int posicaoLetra;
+          public char letra;
+
+          public bool verificaLetra;
+
+          private void ImprimirLetrasParteInferior()
+          {
+               for (int i = posicaoLetra - 1; i >= 0; i--)
+               {
+                    Console.WriteLine("\n");
+
+                    for (int a = 0; a < posicaoLetra - i; a++)
+                    {
+                         Console.Write(" ");
+                    }
+
+                    for (int a = 0; a < 1; a++)
+                    {
+                         Console.Write(letraAlfabeto[i]);
+
+                         for (int g = 0; g <= 2 * i - 2; g++)
+                         {
+                              Console.Write(" ");
+                         }
+
+                         if (i != 0)
+                         {
+                              Console.WriteLine(letraAlfabeto[i]);
+                         }
+                    }
+
+               }
+          }
+
+          private void ImprimirLetrasParteSuperior()
+          {
+               for (int i = 0; i <= posicaoLetra; i++)
+               {
+                    Console.WriteLine("\n");
+
+                    for (int a = 0; a < posicaoLetra - i; a++)
+                    {
+                         Console.Write(" ");
+                    }
+
+                    for (int p = 0; p < 1; p++)
+                    {
+                         Console.Write(letraAlfabeto[i]);
+
+                         for (int k = 0; k <= 2 * i - 2; k++)
+                         {
+                              Console.Write(" ");
+                         }
+
+                         if (i != 0)
+                         {
+                              Console.WriteLine(letraAlfabeto[i]);
+                         }
+                    }
+
+               }
+          }
+
+          public char PegarLetra(string mensagem)
+          {
+               Console.Write(mensagem);
+               letra = Convert.ToChar(Console.ReadLine().ToUpper());
+               return letra;
+          }
+
+          private void VerificarLetra()
+          {
+              verificaLetra = char.IsLetter(letra);
+              letraAlfabeto = alfabeto.ToCharArray();
+
+              posicaoLetra = alfabeto.IndexOf(letra);
+
+               while (letra == 'A' || verificaLetra == false)
+               {
+                    Console.Clear();
+                    Console.Write("Entre com uma letra válida!");
+                    letra = PegarLetra("\nEntre com uma letra diferente de 'a':\n> ");                 
+               }
+          }
+
+          public void ImprimirDiamanteAlfabeto()
+          {
+               VerificarLetra();              
+               ImprimirLetrasParteSuperior();
+               ImprimirLetrasParteInferior();
+          }
+     }
+}
--- P04.DiamanteAlfabético/Program.cs
+++ P04.DiamanteAlfabético/Program.cs
@@ -9,104 +9,19 @@ namespace P04.DiamanteAlfabético
           {
                do
                {
-                    string opcao = GerarMenu();
+                    GerarMenu();
+                    DiamanteAlfabeto diamante1 = new DiamanteAlfabeto();
 
-                    if (opcao == "S")
-                    {
-                         Console.ForegroundColor = ConsoleColor.Red;
-                         Console.WriteLine("\nSaindo...");
-                         Console.ResetColor();
-                         break;
-                    }
+                    diamante1.PegarLetra("\nEntre com uma letra diferente de 'a':\n> ");
 
-                    string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                    char[] letraAlfabeto = alfabeto.ToCharArray();
-
-                    char letra = PegarLetra("\nEntre com uma letra diferente de 'a':\n> ");
-
-                    bool verificaLetra = char.IsLetter(letra);
-
-                    int posicaoLetra = alfabeto.IndexOf(letra);
-
-                    if (letra == 'A' || verificaLetra == false)
-                    {
-                         Console.Clear();
-                         Console.Write("Entre com uma letra válida!");
-                         letra = PegarLetra("\nEntre com uma letra diferente de 'a':\n> ");                       
-                    }
-
-                    else
-                    {
-                         ImprimirLetrasParteSuperior(letraAlfabeto, posicaoLetra);
-                         ImprimirLetrasParteInferior(letraAlfabeto, posicaoLetra);
-                    }
+                    diamante1.ImprimirDiamanteAlfabeto();
 
                     Console.ReadLine();
 
                } while (true);
           }
 
-          #region Funções;
-
-          private static void ImprimirLetrasParteInferior(char[] letraAlfabeto, int posicaoLetra)
-          {
-               for (int i = posicaoLetra - 1; i >= 0; i--)
-               {
-                    Console.WriteLine("\n");
-
-                    for (int a = 0; a < posicaoLetra - i; a++)
-                    {
-                         Console.Write(" ");
-                    }
-
-                    for (int a = 0; a < 1; a++)
-                    {
-                         Console.Write(letraAlfabeto[i]);
-
-                         for (int g = 0; g <= 2 * i - 2; g++)
-                         {
-                              Console.Write(" ");
-                         }
-
-                         if (i != 0)
-                         {
-                              Console.WriteLine(letraAlfabeto[i]);
-                         }
-                    }
-
-               }
-          }
-
-          private static void ImprimirLetrasParteSuperior(char[] letraAlfabeto, int posicaoLetra)
-          {
-               for (int i = 0; i <= posicaoLetra; i++)
-               {
-                    Console.WriteLine("\n");
-
-                    for (int a = 0; a < posicaoLetra - i; a++)
-                    {
-                         Console.Write(" ");
-                    }
-
-                    for (int p = 0; p < 1; p++)
-                    {
-                         Console.Write(letraAlfabeto[i]);
-
-                         for (int k = 0; k <= 2 * i - 2; k++)
-                         {
-                              Console.Write(" ");
-                         }
-
-                         if (i != 0)
-                         {
-                              Console.WriteLine(letraAlfabeto[i]);
-                         }
-                    }
-
-               }
-          }
-
-          static string GerarMenu()
+          static void GerarMenu()
           {
                Console.Clear();
                Console.WriteLine("===============================");
@@ -118,16 +33,13 @@ namespace P04.DiamanteAlfabético
                string opcao;
 
                opcao = Console.ReadLine().ToUpper();
-               return opcao;
-          }
 
-          static char PegarLetra(string mensagem)
-          {
-               Console.Write(mensagem);
-               char letra = Convert.ToChar(Console.ReadLine().ToUpper());
-               return letra;
+               if (opcao == "S")
+               {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.ResetColor();
+                    Environment.Exit(0);
+               }
           }
-
-          #endregion
      }
 }
